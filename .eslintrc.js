4f9261f064cd58e687f58ad464f20380ac6f7dfa
Vincenzo Buono <vincebshock@gmail.com>
2019-02-24 10:28:30 +0100

    Eslint disable indent checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,12 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    'indent': "off", //no indent checking
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // trailing comma
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': ['error', 'never'],
   }
 }
